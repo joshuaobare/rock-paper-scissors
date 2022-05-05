ec58a5d35e70e9d507a05b1ef1696c5751e5c77f
joshuaobare <jobare.obare@gmail.com>
2022-05-05 21:59:28 +0300

    Final program with instructions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,6 @@
 function computerPlay() {
     function getRandomNum(a, b) {
-        return Math.random() * (b - a) + a;
+        return Math.random() * (b - a) + a;   
       }
     
     c=Math.trunc(getRandomNum(1,4));
